aed2477345af066199ae764b2f1083cf1d4193d6
Simon Grätzer <simon@graetzer.org>
2014-07-06 14:41:19 +0200

    Dialog

--- App/MissionMoon/app/src/main/java/de/trivago/missionmoon/HotelDetailFragment.java
+++ App/MissionMoon/app/src/main/java/de/trivago/missionmoon/HotelDetailFragment.java
@@ -1,5 +1,6 @@
 package de.trivago.missionmoon;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.graphics.Bitmap;
@@ -42,14 +43,14 @@ public class HotelDetailFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dia = super.onCreateDialog(savedInstanceState);
-        dia.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return dia;
+        return new AlertDialog.Builder(getActivity()).setTitle("Hotel Buchen")
+                .setView(createView(getActivity().getLayoutInflater()))
+                .setNegativeButton("Abbrechen", null)
+                .create();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View frag = inflater.inflate(R.layout.fragment_hotel_detail, container, false);
+    private View createView(LayoutInflater inflater) {
+        View frag = inflater.inflate(R.layout.fragment_hotel_detail, null);
 
         Hotel hotel = getArguments().getParcelable("hotel");
         TextView tv = (TextView)frag.findViewById(R.id.title);
